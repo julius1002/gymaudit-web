e2eb362b143255ad955ad325ecbaf103608e17fa
Julius <jplaton@ymail.com>
2020-05-22 18:44:24 +0200

    some styles and mat table fixes

--- src/app/components/training-log/log-sets/set-table/set-table.component.ts
+++ src/app/components/training-log/log-sets/set-table/set-table.component.ts
@@ -5,7 +5,7 @@ import { Page } from "src/app/model/page";
 import { Exercise } from "src/app/model/exercise";
 import { SetService } from "src/app/services/set.service";
 import { PageEvent } from "@angular/material/paginator";
-import { switchMap } from 'rxjs/operators';
+import { switchMap } from "rxjs/operators";
 @Component({
   selector: "app-set-table",
   templateUrl: "./set-table.component.html",
@@ -13,33 +13,53 @@ import { switchMap } from 'rxjs/operators';
 })
 export class SetTableComponent implements OnInit {
   @Input() exerciseSubject: Subject<Exercise>;
+  @Input() editing = false;
   setsPage$: Observable<Page<Set>>;
   measureUnitEnum = MeasureUnit;
   index = 0;
   pageSize = 5;
-  displayedColumns = ["reps", "number", "measureUnit", "date"];
+  displayedColumns: String[];
   exercise: Exercise;
 
   constructor(private setService: SetService) {}
 
   ngOnInit(): void {
-    this.exerciseSubject.subscribe(exercise =>  {this.exercise = exercise, this.getExercisesPageFromUnit(this.pageSize, 0, exercise)});
-   
+    this.exerciseSubject.subscribe((exercise) => {
+      (this.exercise = exercise),
+        this.getExercisesPageFromUnit(this.pageSize, 0, exercise);
+    });
+    if (this.editing) {
+      this.displayedColumns = [
+        "reps",
+        "number",
+        "measureUnit",
+        "date",
+        "actions",
+      ];
+    } else {
+      this.displayedColumns = ["reps", "number", "measureUnit", "date"];
+    }
   }
 
   public turn(event: PageEvent) {
-    this.getExercisesPageFromUnit(event.pageSize, event.pageIndex, this.exercise);
+    this.getExercisesPageFromUnit(
+      event.pageSize,
+      event.pageIndex,
+      this.exercise
+    );
     this.index = event.pageIndex;
   }
 
-  private getExercisesPageFromUnit(size: number, page: number, exercise:Exercise): void {
-    this.setsPage$ = 
-     this.setService.getByPage(
-        exercise.unitId,
-        exercise.id,
-        size,
-        page
-      );
-    
+  private getExercisesPageFromUnit(
+    size: number,
+    page: number,
+    exercise: Exercise
+  ): void {
+    this.setsPage$ = this.setService.getByPage(
+      exercise.unitId,
+      exercise.id,
+      size,
+      page
+    );
   }
 }
